378040a7c300df0ceab63bdc68f86703c285a3b7
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-24 23:56:47 +0600

    App to test

---
+++ src/main/java/org/automation/junit5/app/Calculator.java
@@ -0,0 +1,48 @@
+package org.automation.junit5.app;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int add(int[] a) {
+        int result = 0;
+        for (int i : a) {
+            result += i;
+        }
+        return result;
+    }
+
+    public Integer add(Integer... numbers) {
+        Integer result = new Integer(0);
+        for (Integer i : numbers) {
+            result += i;
+        }
+        return result;
+    }
+
+    public <T extends Number> T add(T... t) {
+        Double sum = new Double(0.0);
+        for (T i : t) {
+            sum += i.doubleValue();
+        }
+        return (T) sum;
+    }
+
+    public String add(String... strings){
+        if(null==strings || strings.length<=0)
+        {
+            return "empty_input";
+        }
+        else
+        {
+            StringBuffer sb = new StringBuffer();
+            for (String a:strings){
+                sb.append(a);
+            }
+            return sb.toString();
+        }
+    }
+}
+
