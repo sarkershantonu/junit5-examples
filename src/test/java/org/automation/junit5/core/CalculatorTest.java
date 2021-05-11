a232a791e416ebf00a97d9b77cfae0701022184d
shantonu sarker <sarker.shantonu@gmail.com>
2021-05-11 22:40:29 +0600

    junit 5 example

---
+++ src/test/java/org/automation/junit5/core/CalculatorTest.java
@@ -0,0 +1,25 @@
+package org.automation.junit5.core;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Link;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Test
+@Timeout(2)
+@Epic("Epic Calculator")
+@Link("aLink")
+@Tag("CalT")
+public @interface CalculatorTest {
+}
