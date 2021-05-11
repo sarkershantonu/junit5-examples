7118e3268a4ed5d0239122c3b4143c3341d8ac78
shantonu sarker <sarker.shantonu@gmail.com>
2021-05-11 22:40:47 +0600

    junit 5 example

---
+++ src/test/java/org/automation/junit5/suites/FunctionalTestSuite.java
@@ -0,0 +1,16 @@
+package org.automation.junit5.suites;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+/**
+ * Suite Example
+ */
+@RunWith(JUnitPlatform.class)
+@SelectPackages({"org.automation.junit5.tests"})
+public class FunctionalTestSuite {
+}
