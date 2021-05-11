e02f38f0ead3223ec01348db675539bbbd307a23
shantonu sarker <sarker.shantonu@gmail.com>
2021-05-11 22:41:29 +0600

    junit 5 example

---
+++ src/test/java/org/automation/junit5/tests/parameterized/support/StreamArgumentProvider.java
@@ -0,0 +1,44 @@
+package org.automation.junit5.tests.parameterized.support;
+
+import org.automation.junit5.tests.parameterized.support.VariableStream;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.support.AnnotationConsumer;
+
+import java.lang.reflect.Field;
+import java.util.stream.Stream;
+
+public class StreamArgumentProvider implements ArgumentsProvider, AnnotationConsumer<VariableStream> {
+
+    private String variableName;
+    @Override
+    public void accept(VariableStream variableStream) {
+       variableName = variableStream.value();
+    }
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context)  {
+        return context.getTestClass()
+                .map(this::getField)
+                .map(this::getValue)
+                .orElseThrow(() -> new IllegalArgumentException("failed to load test arguments"));
+
+    }
+    private Field getField(Class<?> klass)  {
+
+        try{ return klass.getDeclaredField(variableName);
+        }catch (Exception e){
+            return null;
+        }
+    }
+    private Stream<Arguments> getValue(Field filed)  {
+        Object val = null;
+        try{
+            val = filed.get(null);
+        }catch (Exception e){
+
+        }
+        return val == null? null : (Stream<Arguments>) val;
+    }
+}
