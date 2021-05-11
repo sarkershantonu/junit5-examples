d3bfd5f33ca1bc6d2084554ab2bcc3c80a903720
shantonu sarker <sarker.shantonu@gmail.com>
2021-05-11 22:40:32 +0600

    junit 5 example

---
+++ src/test/java/org/automation/junit5/core/CalculatorTestBase.java
@@ -0,0 +1,36 @@
+package org.automation.junit5.core;
+
+import io.qameta.allure.listener.StepLifecycleListener;
+import io.qameta.allure.model.StepResult;
+import org.automation.Calculator;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CalculatorTestBase implements StepLifecycleListener {
+    protected Calculator myCal;
+    public Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @BeforeAll
+    public static void initForAllTest(){
+
+    }
+    @AfterAll
+    public static void cleanupForAllTest(){
+
+    }
+
+    @BeforeEach
+    public void initTest()
+    {
+        myCal = new Calculator();
+    }
+
+    @Override
+    public void beforeStepStop(StepResult result) {
+        logger.info("Finished step : {}",result.getName());
+    }
+
+}
